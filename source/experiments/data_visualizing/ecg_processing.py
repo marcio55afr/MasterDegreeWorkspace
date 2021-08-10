1e592b495232136b95fabee0c7faaf109deb1eda
Marcio Antonio <marcio55afr@github.com>
2021-08-10 10:34:02 -0300

    Starting the experiment of couting the ngram words

--- source/experiments/data_visualizing/ecg_processing.py
+++ source/experiments/data_visualizing/ecg_processing.py
@@ -8,8 +8,23 @@ and its importance.
 import sys
 sys.path.append('C:\\Users\\marci\\Desktop\\MasterDegreeWorkspace\\source')
 from experiments.database.ts_handler import get_dataset
-from utils import ResolutionMatrix, ResolutionHandler
+from utils import ResolutionMatrix, ResolutionHandler, NgramExtractor
+from transformations import MSAX
 
+# handling data
 train, test = get_dataset('ECG5000')
+timeseries = train.data
+labels = train.target
 
-ResolutionMatrix.create_matrix(train.iloc[0,0].size)
+# initializating useful objects
+rm = ResolutionMatrix(train.iloc[0,0].size)
+wi, wo =  rm.get_windows_and_words()
+tf = MSAX()
+
+
+ts = timeseries.iloc[0]
+word_seq = tf.transform(ts, wi, wo)
+bob = NgramExtractor.get_bob(word_seq, rm.matrix)
+
+#for ts in timeseries:
+#    MSAX
