74180fb71e14bf512f320623c255ef52256c21c9
Marcio Antonio <marcio55afr@github.com>
2021-09-14 17:11:31 -0300

    Writing the bob as a sparse matrix to save memory and time

--- source/experiments/database/__init__.py
+++ source/experiments/database/__init__.py
@@ -1,6 +1,7 @@
 
 
-__all__ = ['get_dataset','get_Xy_from','DATASET_NAMES']
+__all__ = ['get_dataset','get_Xy_from','DATASET_NAMES', 'read_bob', 'write_bob']
 
 
 from source.experiments.database.ts_handler import get_dataset, get_Xy_from, DATASET_NAMES
+from source.experiments.database.bob_handler import read_bob, write_bob
---
+++ source/experiments/database/bob_handler.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+import numpy as np
+import time
+
+from scipy.sparse import spmatrix, save_npz, load_npz
+
+DATASET_NAMES = ['ECG5000', 'Worms', 'StarLightCurves']
+BOB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bob/")
+EXTENSION = '.csv'
+
+CONFIG_PATH = '{}/{}/WordLen_{}/'.format
+# "dataset_name"/"discretization"/WordLen_6/bob_"split".csv
+
+def write_bob( bob: spmatrix , samples_id, words, dataset_name, discretization, wordlen, split):
+    
+    config_path = CONFIG_PATH(dataset_name, discretization, wordlen)
+    folder_path = BOB_PATH+config_path
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    #print('Writing the bag of bags...')
+    bob_path = folder_path+'bob_{}.npz'.format(split)
+    index_path = folder_path+'indices_{}.csv'.format(split)
+    column_path = folder_path+'columns_{}.csv'.format(split)
+    
+    samples_id = pd.Series(samples_id)
+    words = pd.Series(words)
+    
+    save_npz(bob_path, bob)
+    samples_id.to_csv(index_path, index=False)
+    words.to_csv(column_path, index=False)
+
+def read_bob(dataset_name, discretization, wordlen, split):
+    
+    config_path = CONFIG_PATH(dataset_name, discretization, wordlen)
+    folder_path = BOB_PATH+config_path
+    bob_path = folder_path+'bob_{}.npz'.format(split)
+    index_path = folder_path+'indices_{}.csv'.format(split)
+    column_path = folder_path+'columns_{}.csv'.format(split)
+    
+    if not os.path.isfile(bob_path) or not os.path.isfile(index_path) or not os.path.isfile(column_path):
+        raise RuntimeError("Tried to read an inexistent file with path: ",
+                           bob_path,
+                           index_path,
+                           column_path)
+    
+    #print('Reading the bag of bags...')
+    bob = load_npz(bob_path)
+    indices = pd.read_csv(index_path, index_col = False, squeeze=True)
+    columns = pd.read_csv(column_path, index_col = False, squeeze=True)
+    return bob, indices.values, columns.values
+    
