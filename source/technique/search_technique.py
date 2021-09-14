2ac0553aef53ce77de32f651f6469beda6991a82
Marcio Antonio <marcio55afr@github.com>
2021-09-14 17:15:16 -0300

    Extracts all samples at once, add the multiresolution framework and adapt to work with sparse matrix

--- source/technique/search_technique.py
+++ source/technique/search_technique.py
@@ -10,9 +10,15 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 
 from source.utils import ResolutionHandler, ResolutionMatrix, NgramExtractor
-from source.transformations import MSAX
+from source.transformations import MultiresolutionFramework
+
 from source.technique.word_ranking import WordRanking
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+
 from source.technique.resolution_selector import ResolutionSelector
+
+from source.experiments.database import read_bob, write_bob
 
 
 class SearchTechnique(BaseClassifier):
@@ -21,12 +27,13 @@ class SearchTechnique(BaseClassifier):
     
     def __init__(self,
                  ts_length,
+                 word_length = 6,
+                 alphabet_size = 4,
                  word_ranking_method = 'chi2',
                  word_selection = 'best n words', # ['p threshold', 'best n words']
                  p_threshold = 0.05,
                  n_words = 120,
-                 alphabet_size = 4,
-                 word_length = 6,
+                 verbose = True,
                  num_windows = None,
                  max_window_length = None,
                  random_state = None):
@@ -41,21 +48,31 @@ class SearchTechnique(BaseClassifier):
         self.word_selection = word_selection
         self.p_threshold = p_threshold
         self.n_words = n_words
+        self.verbose = verbose
+                    
+        if self.verbose:
+            print('Initializating the classifier...\n')
         
-        
-        self.alphabet_size = alphabet_size
         self.word_length = word_length
-        self.num_windows = num_windows
-        self.max_window_length = max_window_length
+        self.alphabet_size = alphabet_size
+        self.num_windows = 20
+        self.max_window_length = 50
         
         self.random_state = random_state
-        self.resolution_matrix = ResolutionMatrix(ts_length,
-                                                  word_length,
-                                                  num_windows,
-                                                  max_window_length)
-        self.discretizer = MSAX(alphabet_size)
+        # TODO implement the function create_matrix in the ResolutionHandler
+        # no need for this ResolutionMatrix class
+        self.resolution_matrix = ResolutionMatrix(self.ts_length,
+                                                  self.word_length,
+                                                  self.num_windows,
+                                                  self.max_window_length)
         
-        self.clf = LogisticRegression(max_iter=500,
+        self.discretization = "Multidomain"
+        self.framework = MultiresolutionFramework(self.resolution_matrix.matrix,
+                                                  word_len=self.word_length,
+                                                  alphabet_size=self.alphabet_size,
+                                                  discretization=self.discretization)
+        
+        self.clf = LogisticRegression(max_iter=5000,
                                       random_state=random_state)
         
         self.selected_words = set()
@@ -63,13 +80,34 @@ class SearchTechnique(BaseClassifier):
     
     def fit(self, data, labels):
         
-        if( data.shape[0] != labels.shape[0] ):
+        if type(data) != pd.DataFrame:
+            raise TypeError("The data must be a type of pd.DataFrame."
+                            " It was received as {}".format(type(data)))
+                            
+        if type(labels) != pd.Series:
+            raise TypeError("The data must be a type of pd.Series."
+                            " It was received as {}".format(type(labels)))
+                            
+        if data.shape[0] != labels.shape[0]:
             raise RuntimeError('The labels isn\'t compatible with the data received')
         
+        if labels is not None:
+            if (data.index.values != labels.index.values).any():
+                raise RuntimeError("The samples indices must be the equal to "
+                                   "the labels indices")
+        
+        
+        if self.verbose:
+            print('Fitting the Classifier with data...\n')
+        
         # testing one specific dataset to avoid rework
-        folder_path = 'C:/Users/marci/Desktop/MasterDegreeWorkspace/source/experiments/data_visualizing/worms/WordLen_6/'
-        bob_path = folder_path+'/bag_of_bags_train.csv'
-        bob = pd.read_csv(bob_path)
+        try:
+            bob, samples_id, words = read_bob( 'ecg', self.discretization, self.word_length, 'train' )
+            self.framework.fit(data, labels)
+        except Exception as e:
+            print(e)
+            bob, samples_id, words = self._extract_bob_from(data, labels)
+            write_bob( bob, samples_id, words, 'ecg', self.discretization, self.word_length, 'train' )
         
         # testing one specific resolution
         #matrix = self.resolution_matrix.matrix
@@ -77,49 +115,56 @@ class SearchTechnique(BaseClassifier):
         #ngram = matrix[matrix[window] > 0].index[0]
         #bob = bob[ bob.window == window]
         #bob = bob[ bob.ngram == ngram]
-                
-        #data = data.squeeze()                
+                                
         #bob = self._extract_bob_from(data, labels)
         
-        feature_vec = bob.pivot(index='sample', columns='ngram word', values='frequency')
-        feature_vec = feature_vec.fillna(0)
-        del(bob)
-
-        word_rank = pd.DataFrame(index = feature_vec.columns.values)
-        rank_value, p = WordRanking.get_ranking( self.word_ranking_method, feature_vec, labels )
+        
+        rank_value, p = chi2(bob, labels)
+        word_rank = pd.DataFrame(index = words)
         word_rank['rank'] = rank_value
         word_rank['p'] = p
+        word_rank.index.name = 'word'
         
-        resolution = word_rank.index.map( lambda x: ResolutionHandler.get_resolution_from(x) )
-        word_rank['resolution'] = resolution        
-        best_reso = ResolutionSelector.get_best_resolution_max(word_rank)
+        #resolution = word_rank.index.map( lambda x: ResolutionHandler.get_resolution_from(x) )
+        #word_rank['resolution'] = resolution        
+        #best_reso = ResolutionSelector.get_best_resolution_max(word_rank)
         
-        word_rank.index.name = 'ngram word'
-        word_rank = word_rank.reset_index().set_index('resolution')
-        word_rank = word_rank.loc[best_reso].reset_index().set_index('ngram word')
+        #word_rank = word_rank.reset_index().set_index('resolution')
+        #word_rank = word_rank.loc[best_reso].reset_index().set_index('ngram word')
         best_words = []
         if( self.word_selection == 'p threshold' ):
             best_words = word_rank[word_rank['p'] <= self.p_threshold].index.values
+            print('words above the p threshold: ', len(best_words))
         if( self.word_selection == 'best n words' ):
             best_words = word_rank.sort_values('rank', ascending=False).iloc[0:self.n_words].index.values
 
-        self.selected_words = set(best_words)
+        self.selected_words = sorted(best_words)
         
+        feature_vec = pd.DataFrame()
+        indices = word_rank.index.get_indexer(self.selected_words)
+        for i,j in zip(range(indices.size),indices):
+            feature_vec[self.selected_words[i]] = bob.getcol(j).toarray().squeeze()
+            
         s = time.time()
-        self.clf.fit(feature_vec[best_words], labels)
+        self.clf.fit(feature_vec, labels)
         spend = time.time() - s
-        print('training time: {}'.format(spend))
+        print('LR training time: {}'.format(spend))
         
         self._is_fitted = True
     
     def predict(self, data):
         
+        if self.verbose:
+            print('Predicting data with the Classifier...\n')
+        
         self.check_is_fitted()
         
         # testing one specific dataset to avoid rework
-        folder_path = 'C:/Users/marci/Desktop/MasterDegreeWorkspace/source/experiments/data_visualizing/worms/WordLen_6/'
-        bob_path = folder_path+'/bag_of_bags_test.csv'
-        bob = pd.read_csv(bob_path)
+        try:
+            bob, samples_id, words = read_bob( 'ecg', self.discretization, self.word_length, 'test' )
+        except:
+            bob, samples_id, words  = self._extract_bob_from(data)
+            write_bob( bob, samples_id, words, 'ecg', self.discretization, self.word_length, 'test' )
         
         
         # testing one specific resolution
@@ -128,56 +173,45 @@ class SearchTechnique(BaseClassifier):
         #ngram = matrix[matrix[window] > 0].index[0]
         #bob = bob[ bob.window == window]
         #bob = bob[ bob.ngram == ngram]
-        
-        #data = data.squeeze()                
+                     
         #bob = self._extract_bob_from(data)
         
-        test_words = set(bob['ngram word'].unique())
-        first_filtering = self.selected_words.intersection(test_words)
-        print('intersecting words:', len(first_filtering))
-        filtered_bob = bob.set_index('ngram word').loc[first_filtering].reset_index()
+        intersecting_words = []
         
-        all_samples = set(bob['sample'].unique())
-        filtered_samples = set(filtered_bob['sample'].unique())
-        missing_samples = all_samples - filtered_samples
+        words_index = pd.Index(words)
+        indices = words_index.get_indexer(self.selected_words)
+        feature_vec = pd.DataFrame(index=samples_id)
+        for i,j in zip(range(indices.size),indices):
+            if j>=0:
+                feature_vec[self.selected_words[i]] = bob.getcol(j).toarray().squeeze()
+                intersecting_words.append(self.selected_words[i])
+            else:
+                feature_vec[self.selected_words[i]] = 0
         
-        sample_group = bob.groupby('sample').head(1)
-        for sample in missing_samples:
-            filtered_bob = filtered_bob.append( sample_group[sample_group['sample'] == sample] )
+        print('Intersecting words: {}'.format( len(intersecting_words)) )
+        print(intersecting_words)
         
-        feature_vec = filtered_bob.pivot(index='sample', columns='ngram word', values='frequency')
-        feature_vec = feature_vec.fillna(0)
+        #all_samples = set(bob['sample'].unique())
+        #filtered_samples = set(filtered_bob['sample'].unique())
+        #missing_samples = all_samples - filtered_samples
         
-        for word in self.selected_words:
-            if word not in feature_vec.columns:
-                feature_vec[word] = 0
+        #sample_group = bob.groupby('sample').head(1)
+        #for sample in missing_samples:
+        #    filtered_bob = filtered_bob.append( sample_group[sample_group['sample'] == sample] )
         
-        filtered_vec = feature_vec[self.selected_words]
+        #feature_vec = filtered_bob.pivot(index='sample', columns='ngram word', values='frequency')
+        #feature_vec = feature_vec.fillna(0)
         
-        return self.clf.predict(filtered_vec)
+        
+        #filtered_vec = feature_vec[self.selected_words]
+        
+        return self.clf.predict(feature_vec)
         
     
-    def _extract_bob_from(self, timeseries_set, labels=None):
+    def _extract_bob_from(self, data, labels=None):        
         
-        rm = self.resolution_matrix
-        windows =  rm.get_windows()
+        word_sequences = self.framework.transform(data) if labels is None \
+            else self.framework.fit_transform(data, labels)
+        bag_of_bags, samples_id, words = NgramExtractor.get_bob(word_sequences, self.resolution_matrix.matrix)
         
-        i=0
-        n_samples = timeseries_set.shape[0]
-        bag_of_bags = pd.DataFrame()
-        print("Generating the bag of bags")
-        print("__________")
-        for ts in timeseries_set:
-            if(i%(n_samples/10) == 0):
-                print('#',end='')
-            word_seq = self.discretizer.transform(ts, windows)
-            bob = NgramExtractor.get_bob(word_seq, rm.matrix)
-            bob['sample'] = i
-            i+=1
-            bag_of_bags = pd.concat([bag_of_bags,bob], axis=0, join='outer', ignore_index=True)
-        
-        
-        if labels is not None:
-            bag_of_bags['label'] = labels.loc[bag_of_bags['sample']].values
-        
-        return bag_of_bags
+        return bag_of_bags, samples_id, words
