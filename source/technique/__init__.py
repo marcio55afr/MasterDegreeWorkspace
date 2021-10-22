e87605e5ed595a61af329a2a7c53efe8502b0118
Marcio Antonio <marcio55afr@github.com>
2021-10-22 15:45:28 -0300

    Adding the random classifier to observe the naiviest results

--- source/technique/__init__.py
+++ source/technique/__init__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 
-__all__ = ['SearchTechnique',
+__all__ = ['RandomClassifier',
+           'SearchTechnique',
            'SearchTechniqueCV',
            'SearchTechnique_CV_RFSF',
-           'SearchTechnique_SG',
-           'SearchTechnique_SG_RR',
            'SearchTechnique_KWS',
            'SearchTechnique_SG_CLF',
-           'SearchTechnique_MR']
+           'SearchTechnique_MR',
+           'SearchTechnique_Ngram']
 
 
 
+from source.technique.random_classifier import RandomClassifier
 from source.technique.search_technique import SearchTechnique
 from source.technique.search_technique_CV import SearchTechniqueCV
 from source.technique.search_technique_CV_RFSF import SearchTechnique_CV_RFSF
-from source.technique.search_technique_SG import SearchTechnique_SG
-from source.technique.search_technique_SG_RR import SearchTechnique_SG_RR
 from source.technique.search_technique_KWS import SearchTechnique_KWS
 from source.technique.search_technique_SG_CLF import SearchTechnique_SG_CLF
 from source.technique.search_technique_MR import SearchTechnique_MR
+from source.technique.search_technique_Ngram import SearchTechnique_Ngram
 
 #from source.technique.word_ranking import WordRanking
 #from source.technique.resolution_selector import ResolutionSelector
---
+++ source/technique/random_classifier.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+sys.path.append('C:/Users/marci/Desktop/MasterDegreeWorkspace/source')
+sys.path.append('C:/Users/marci/Desktop/MasterDegreeWorkspace')
+
+import random
+import pandas as pd
+import numpy as np
+from sktime.classification.base import BaseClassifier
+
+
+class RandomClassifier(BaseClassifier):
+    """
+        Random classifier to compare the naiviest approach to classifier
+        acording to the Class Probability of the train data.
+    
+    """    
+    
+    def __init__(self, verbose=False, random_state=None):
+        
+        
+        self.labels = None
+        self.verbose = verbose
+        self.random_state = random_state
+    
+    
+    def fit(self, data, labels):
+        
+        if self.verbose:
+            print('Fitting the Classifier...\n')
+        self.labels = pd.Series(labels)
+        self._is_fitted = True
+    
+    def predict(self, data):
+        
+        self.check_is_fitted()
+        if self.verbose:
+            print('Predicting data with the Classifier...\n')
+
+        n_samples =  data.shape[0]
+        _random_state = None
+        if self.random_state is not None:
+            _random_state = n_samples + self.random_state
+        y_pred = self.labels.sample(n_samples, replace=True, random_state=_random_state)   
+        y_pred.index = data.index
+        return y_pred.values
+    
+    def predict_proba(self, data):
+        
+        if self.verbose:
+            print('Predicting data with the Classifier...\n')
+        
+        self.check_is_fitted()
+        
+        n_samples =  data.shape[0]
+        _random_state = None
+        if self.random_state is not None:
+            _random_state = n_samples + self.random_state
+        y_pred = self.labels.sample(n_samples, replace=True, random_state=_random_state)   
+        
+        y_proba = pd.DataFrame(0, index=range(n_samples), columns=self.labels.unique())
+        for i in range(n_samples):
+            y_proba.loc[i, y_pred[i]] = 1
+        
+        
+        return y_proba.values
