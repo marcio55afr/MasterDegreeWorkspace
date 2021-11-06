5ee7d39ac1be1a9f308772f68d0c980523539a3f
Marcio Antonio <marcio55afr@github.com>
2021-11-06 16:15:14 -0300

    changin init

--- source/technique/__init__.py
+++ source/technique/__init__.py
@@ -4,7 +4,7 @@
 __all__ = ['RandomClassifier',
            'SearchTechnique',
            'SearchTechniqueCV',
-           'SearchTechnique_CV_RFSF',
+           'SearchTechnique_CV_RSFS',
            'SearchTechnique_KWS',
            'SearchTechnique_SG_CLF',
            'SearchTechnique_MD',
@@ -16,7 +16,7 @@ __all__ = ['RandomClassifier',
 from source.technique.random_classifier import RandomClassifier
 from source.technique.search_technique import SearchTechnique
 from source.technique.search_technique_CV import SearchTechniqueCV
-from source.technique.search_technique_CV_RFSF import SearchTechnique_CV_RFSF
+from source.technique.search_technique_CV_RFSF import SearchTechnique_CV_RSFS
 from source.technique.search_technique_KWS import SearchTechnique_KWS
 from source.technique.search_technique_SG_CLF import SearchTechnique_SG_CLF
 from source.technique.search_technique_MD import SearchTechnique_MD
