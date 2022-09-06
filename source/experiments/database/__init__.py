f22e594965840a772de4c8dfaea8446dd4a00419
marciofreitas <marcio55afr@gmail.com>
2022-09-06 05:02:49 -0300

    Organizing local python packages

--- source/experiments/database/__init__.py
+++ source/experiments/database/__init__.py
@@ -1,7 +1,7 @@
 
 
-__all__ = ['get_dataset','get_Xy_from','DATASET_NAMES', 'read_bob', 'write_bob']
+__all__ = ['get_dataset', 'get_train_test_split', 'DATASET_NAMES', 'read_bob', 'write_bob']
 
 
-from source.experiments.database.ts_handler import get_dataset, get_Xy_from, DATASET_NAMES
+from source.utils.handlers.ts_handler import DATASET_NAMES
 from source.experiments.database.bob_handler import read_bob, write_bob
--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -2,7 +2,7 @@
 __all__ = ['ResolutionHandler', 'ResolutionMatrix', 'NgramExtractor',
            'draw_cd_diagram', 'calculate_efficiency']
 
-from source.utils.resolution_handler import ResolutionHandler
+from source.utils.handlers.resolution_handler import ResolutionHandler
 from source.utils.resolution_matrix import ResolutionMatrix
 from source.utils.ngram_extractor import NgramExtractor
 from source.utils.critical_diagram import draw_cd_diagram
