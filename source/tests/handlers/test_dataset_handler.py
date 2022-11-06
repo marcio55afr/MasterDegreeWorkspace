21253c1585ad6bbfd129de41b5a533aab59960c0
marciofreitas <marcio55afr@gmail.com>
2022-11-06 10:04:49 -0300

    Testing the new dataset handler

---
+++ source/tests/handlers/test_dataset_handler.py
@@ -0,0 +1,11 @@
+import unittest
+import random
+from source.utils import DatasetHandler
+
+
+class TestFunction_get_dataset(unittest.TestCase):
+
+    def test_execution(self):
+        data = DatasetHandler.get_dataset('SmoothSubspace')
+
+        assert all([split is not None for split in data])
