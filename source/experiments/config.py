c9f59a51b7026565bdbc9eedde07a3d8755a766d
marciofreitas <marcio55afr@gmail.com>
2022-11-06 10:17:14 -0300

    init files to express folders as packages and creating config files to hold important paths

---
+++ source/experiments/config.py
@@ -0,0 +1,3 @@
+import os
+
+OUTPUT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".output/")
--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -1,9 +1,11 @@
 
 __all__ = ['ResolutionHandler', 'ResolutionMatrix', 'NgramExtractor',
-           'draw_cd_diagram', 'calculate_efficiency']
+           'draw_cd_diagram', 'calculate_efficiency', 'DatasetHandler', 'TsHandler']
 
 from source.utils.handlers.resolution_handler import ResolutionHandler
 from source.utils.resolution_matrix import ResolutionMatrix
 from source.utils.ngram_extractor import NgramExtractor
 from source.utils.critical_diagram import draw_cd_diagram
 from source.utils.scoring import calculate_efficiency
+from source.utils.handlers.dataset_handler import DatasetHandler
+from source.utils.handlers.ts_handler import TsHandler
