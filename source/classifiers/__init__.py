27abb0d0ef0ba08d87d7a9a9c4d0f64cec3d9bf5
marciofreitas <marcio55afr@gmail.com>
2023-01-04 12:23:28 -0300

    Creating python packages for folder organization

---
+++ source/classifiers/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from source.classifiers.random_classifier import RandomClassifier
+from source.classifiers.classifier_3m import Classifier3M
+
+__all__ = ['RandomClassifier',
+           'Classifier3M']
+# 'SearchTechnique',
+# 'SearchTechniqueCV',
+# 'SearchTechnique_CV_RSFS',
+# 'SearchTechnique_KWS',
+# 'SearchTechnique_SG_CLF',
+# 'SearchTechnique_MD',
+# 'SearchTechnique_Ngram',
+# 'SearchTechnique_NgramResolution',
+# 'SearchTechnique_Ensemble']
+
+
+# from source.classifiers.variants.search_technique_KWS import SearchTechnique_KWS
+# from source.classifiers.word_ranking import WordRanking
+# from source.classifiers.resolution_selector import ResolutionSelector
--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -7,5 +7,5 @@ from source.utils.resolution_matrix import ResolutionMatrix
 from source.utils.ngram_extractor import NgramExtractor
 from source.utils.critical_diagram import draw_cd_diagram
 from source.utils.scoring import calculate_efficiency
-from source.utils.handlers.dataset_handler import DatasetHandler
 from source.utils.handlers.ts_handler import TsHandler
+from source.utils.handlers.dataset_handler import DatasetHandler
