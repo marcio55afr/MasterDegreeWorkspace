7ea4bfc352b2afb1fd8b6089ce99de86b46b44a8
marciofreitas <marcio55afr@gmail.com>
2023-01-04 12:18:38 -0300

    Testing data reading as array and DataHandler functions

--- source/tests/handlers/test_dataset_handler.py
+++ source/tests/handlers/test_dataset_handler.py
@@ -6,6 +6,6 @@ from source.utils import DatasetHandler
 class TestFunction_get_dataset(unittest.TestCase):
 
     def test_execution(self):
-        data = DatasetHandler.get_dataset('SmoothSubspace')
+        data = DatasetHandler.get_split_dataset('SmoothSubspace')
 
         assert all([split is not None for split in data])
