ea6fa9a76434a8aca467a73139e87fb5c2f97b91
marciofreitas <marcio55afr@gmail.com>
2023-01-04 12:12:28 -0300

    Rewriting the name of all ucr 2018 datasets and creating 3 groups for experiments

--- source/data/config.py
+++ source/data/config.py
@@ -10,7 +10,40 @@ UNIVARIATE_HDF_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 
 EXTENSION = '.h5'
 
+LONGEST_DATASETS = ['Strawberry', 'MiddlePhalanxOutlineCorrect', 'Wafer', 'TwoPatterns', 'PhalangesOutlinesCorrect']
+# Crop too large for tuning parameters...
+WIDEST_DATASETS = ['ACSF1', 'CinCECGTorso', 'InlineSkate', 'HouseTwenty', 'Rock']
+LARGEST_DATASETS = ['NonInvasiveFetalECGThorax2', 'FordB', 'HandOutlines']
+# PigCVP, PigArtPressure and PigAirwayPressure has less than 5 objects per class
+
+
+DATA_INFO = 'source/data/info.csv'
+
+# Univariate equal length datasets
 DATASET_NAMES = [
+    "ACSF1", "Adiac", "ArrowHead", "Beef", "BeetleFly", "BirdChicken", "BME",
+    "Car",     "CBF",     "Chinatown",     "ChlorineConcentration",     "CinCECGTorso",     "Coffee",     "Computers",
+    "CricketX",     "CricketY",     "CricketZ",     "Crop",     "DiatomSizeReduction", "DistalPhalanxOutlineCorrect",
+    "DistalPhalanxOutlineAgeGroup",     "DistalPhalanxTW",     "Earthquakes",     "ECG200", "ECG5000", "ECGFiveDays",
+    "ElectricDevices",     "EOGHorizontalSignal",     "EOGVerticalSignal",     "EthanolLevel", "FaceAll", "FaceFour",
+    "FacesUCR",     "FiftyWords",     "Fish",     "FordA",     "FordB",     "FreezerRegularTrain", "FreezerSmallTrain",
+    "Fungi",     "GunPoint",     "GunPointAgeSpan",     "GunPointMaleVersusFemale",    "GunPointOldVersusYoung", "Ham",
+    "HandOutlines",     "Haptics",     "Herring",     "HouseTwenty", "InlineSkate",     "InsectEPGRegularTrain",
+    "InsectEPGSmallTrain",     "InsectWingbeatSound",     "ItalyPowerDemand",  "LargeKitchenAppliances", "Lightning2",
+    "Lightning7",     "Mallat",     "Meat",     "MedicalImages",     "MiddlePhalanxOutlineCorrect",
+    "MiddlePhalanxOutlineAgeGroup",     "MiddlePhalanxTW",     "MixedShapesRegularTrain",     "MixedShapesSmallTrain",
+    "MoteStrain",     "NonInvasiveFetalECGThorax1",     "NonInvasiveFetalECGThorax2",     "OliveOil", "OSULeaf",
+    "PhalangesOutlinesCorrect", "Phoneme", "PigAirwayPressure", "PigArtPressure", "PigCVP", "Plane", "PowerCons",
+    "ProximalPhalanxOutlineCorrect", "ProximalPhalanxOutlineAgeGroup", "ProximalPhalanxTW", "RefrigerationDevices",
+    "Rock", "ScreenType", "SemgHandGenderCh2", "SemgHandMovementCh2", "SemgHandSubjectCh2", "ShapeletSim", "ShapesAll",
+    "SmallKitchenAppliances", "SmoothSubspace", "SonyAIBORobotSurface1", "SonyAIBORobotSurface2", "StarLightCurves",
+    "Strawberry", "SwedishLeaf", "Symbols", "SyntheticControl", "ToeSegmentation1", "ToeSegmentation2", "Trace",
+    "TwoLeadECG", "TwoPatterns", "UMD", "UWaveGestureLibraryAll", "UWaveGestureLibraryX", "UWaveGestureLibraryY",
+    "UWaveGestureLibraryZ", "Wafer", "Wine", "WordSynonyms", "Worms", "WormsTwoClass", "Yoga"
+]
+
+SHORTER_DATASETS_NAMES = [
+    'ACSF1',
     'SmoothSubspace',
     'Chinatown',
     'ItalyPowerDemand',
