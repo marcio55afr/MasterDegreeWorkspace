8b0744b2ce3bb15f877738bfa70d1dc2b364c25e
marciofreitas <marcio55afr@gmail.com>
2023-01-04 12:09:23 -0300

    Creating main to run process and experiments

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from source.experiments.variant_searching.variant_searching import run_guided_path
+import pandas as pd
 
-exp = '1'  # True
+from source.experiments.hyperparam_tuning.hyperparam_tuning import run_3m_tuning_process
+
+experiment_results = None  # used to get experiment results
+exp = '2'
 while exp:
     print(
         """Which experiment do you want run:
         
         1- Univariate Time Series Classification Benchmark
-        2-
+        2- 3M's hyperparameters tuning
         3- 
         0- Exit
 """
@@ -15,8 +18,11 @@ while exp:
         break
 
     elif exp == '1':
-        run_guided_path()
+        print('Not yet implemented')
+        # run_guided_path()
 
+    elif exp == '2':
+        experiment_results = run_3m_tuning_process()
     else:
         print("Choose a valid option\n\n")
 
